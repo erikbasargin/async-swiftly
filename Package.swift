e29a395f83a6e936c14e6090496f55aafc430a7c
Erik Basargin <basargin.erik@gmail.com>
2026-09-12 23:24:05 +0100

    Configure project basis
    * Use swift 6.4
    * Enable ApproachableConcurrency &  ExistentialAny features
    * Add license header template
    * Add .swift-format
    * Disable API breakage check
    * Fix formatting

--- Package.swift
+++ Package.swift
@@ -1,23 +1,32 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+
+var defaultSwiftSettings: [SwiftSetting] {
+    [
+        .enableUpcomingFeature("ApproachableConcurrency"),
+        .enableUpcomingFeature("ExistentialAny"),
+    ]
+}
 
 let package = Package(
     name: "async-swiftly",
     products: [
         .library(
             name: "AsyncSwiftly",
-            targets: ["AsyncSwiftly"]
-        ),
+            targets: ["AsyncSwiftly"],
+        )
     ],
     targets: [
         .target(
-            name: "AsyncSwiftly"
+            name: "AsyncSwiftly",
+            swiftSettings: defaultSwiftSettings,
         ),
         .testTarget(
             name: "AsyncSwiftlyTests",
-            dependencies: ["AsyncSwiftly"]
+            dependencies: ["AsyncSwiftly"],
+            swiftSettings: defaultSwiftSettings,
         ),
-    ]
+    ],
 )
--- Tests/AsyncSwiftlyTests/AsyncSwiftlyTests.swift
+++ Tests/AsyncSwiftlyTests/AsyncSwiftlyTests.swift
@@ -1,4 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the async-swiftly open source project
+//
+// Copyright (c) 2026 Erik Basargin and the async-swiftly project authors
+// SPDX-License-Identifier: MIT
+//
+// See LICENSE for license information
+//
+//===----------------------------------------------------------------------===//
+
 import Testing
+
 @testable import AsyncSwiftly
 
 @Test func example() async throws {
