e24a0f0604ad0a926a63c958c2a680555af92a2c
Erik Basargin <basargin.erik@gmail.com>
2026-09-12 23:01:18 +0100

    Add license header template

--- Tests/AsyncSwiftlyTests/AsyncSwiftlyTests.swift
+++ Tests/AsyncSwiftlyTests/AsyncSwiftlyTests.swift
@@ -1,3 +1,14 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the async-swiftly open source project
+//
+// Copyright (c) 2026 Erik Basargin and the async-swiftly project authors
+// SPDX-License-Identifier: MIT
+//
+// See LICENSE for license information
+//
+//===----------------------------------------------------------------------===//
+
 import Testing
 @testable import AsyncSwiftly
 
