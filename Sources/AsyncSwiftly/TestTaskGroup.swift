fc035acba27c4c91751712212cd6c3f580c17c14
Erik Basargin <basargin.erik@gmail.com>
2026-09-14 11:51:23 +0100

    chore(test-task-group): add TestTaskGroup scaffolding

--- Sources/AsyncSwiftly/TestTaskGroup.swift
+++ Sources/AsyncSwiftly/TestTaskGroup.swift
@@ -9,10 +9,4 @@
 //
 //===----------------------------------------------------------------------===//
 
-import Testing
-
-@testable import AsyncSwiftly
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
+public func withTestTaskGroup(body: () -> Void) {}
