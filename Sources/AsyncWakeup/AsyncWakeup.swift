2fcc6ce8f8ce3a8670879b9c8432ef6538444839
Erik Basargin <basargin.erik@gmail.com>
2026-09-17 09:34:54 +0100

    chore(async-wakeup): add scaffolding

---
+++ Sources/AsyncWakeup/AsyncWakeup.swift
@@ -0,0 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the async-swiftly open source project
+//
+// Copyright (c) 2026 Erik Basargin and the async-swiftly project authors
+// SPDX-License-Identifier: MIT
+//
+// See LICENSE for license information
+//
+//===----------------------------------------------------------------------===//
+
+public struct AsyncWakeup: Sendable {
+    
+    public init() {}
+    
+    public func signal() {}
+    
+    public func wait() async throws {}
+}
